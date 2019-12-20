c07b99c83f3d05ca9a8f30e680ac58f689b2ab3d
BKoziel <bartlomiej_koziel@hotmail.com>
2019-12-20 22:05:21 +0100

    login gui 1.1

--- app/src/main/java/com/example/wastic/LoginActivity.java
+++ app/src/main/java/com/example/wastic/LoginActivity.java
@@ -2,9 +2,17 @@ package com.example.wastic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
+    TextView registerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +20,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        registerButton = (TextView) findViewById(R.id.textViewRegister);
 
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
 
     }
 
